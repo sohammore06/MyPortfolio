ed37675d936699c17d43e6031790cb1c33bd4af2
Soham More <sohammore2372@gmail.com>
2026-03-20 02:25:30 +0530

    scroll fix for blog page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useLayoutEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
 import PortfolioHome from "./pages/PortfolioHome";
 import BlogArticlePage from "./pages/BlogArticlePage";
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+  }, [pathname]);
+
+  return null;
+};
+
+const AppRoutes = () => (
+  <>
+    <ScrollToTop />
+    <Routes>
+      <Route path="/" element={<PortfolioHome />} />
+      <Route path="/blog/:slug" element={<BlogArticlePage />} />
+    </Routes>
+  </>
+);
+
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<PortfolioHome />} />
-        <Route path="/blog/:slug" element={<BlogArticlePage />} />
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   );
 };
